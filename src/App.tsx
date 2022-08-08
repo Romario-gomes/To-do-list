532fbcdc61597b350fcdc04a1dc55a2b77cabc20
Romario Work <romario.g@estrela10.com.br>
2022-08-08 20:46:34 -0300

    Feat(Initial To-do List): Add Base Project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { GlobalStyle } from './styles/global';
+
+import { Header } from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header ></Header>
+      <GlobalStyle></GlobalStyle>
+    </>
+    
+  )
+}
+
+export default App
